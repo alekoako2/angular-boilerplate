a323680a3d1a79efabf2fe8795f9607d8cf6ffc0
Aleksandre Phatsatsia <aleksandrephatsatsia@Aleksandres-iMac.local>
2020-06-19 14:48:04 +0400

    added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
 import { HashLocationStrategy, LocationStrategy } from '@angular/common'
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
     {
       useClass: HashLocationStrategy,

02575ca061f592460610e908e5674d753a10ac22
Aleksandre Phatsatsia <aleksandrephatsatsia@Aleksandres-iMac.local>
2020-06-19 14:55:19 +0400

    added hmr

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common'
 
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './pages/home/home.component'
-import { HmAutocompleteOverviewModule } from '@shared/components/hm-autocomplete-overview/hm-autocomplete-overview.module'
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, HmAutocompleteOverviewModule],
+  imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
